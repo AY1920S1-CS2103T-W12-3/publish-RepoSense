9fe1e63ae3f2fea59eef148a4ba3a87223bb5ca1
Joel <42489904+joloong@users.noreply.github.com>
2019-10-20 18:11:57 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -14,4 +14,4 @@
 ## Examples of other valid options; For more, please view the user guide
 ### java -jar RepoSense.jar --repo https://github.com/reposense/RepoSense.git
 
-java -jar RepoSense.jar --config ./configs --repo https://github.com/AY1920S1-CS2103T-W12-3/main.git
+java -jar RepoSense.jar --repo https://github.com/AY1920S1-CS2103T-W12-3/main.git
